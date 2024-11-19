2425f0ca64c111ced8ac81d91db608d81eb18c7d
Emmanuel I. Obi <withtwoemms@gmail.com>
2024-11-19 13:53:19 -0600

    adds jaaspr.models

---
+++ jaaspr/models.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+from typing import Generic, Optional, TypeVar
+
+T = TypeVar('T')
+
+@dataclass
+class Job(Generic[T]):
+    job_id: str
+    status: str
+    result: Optional[T] = None
+
+
+@dataclass
+class FakeResult():
+    state: str
+    value: str
