5bddc860402cac573a4835c8d90efc9000f99e59
Emmanuel I. Obi <withtwoemms@gmail.com>
2024-11-20 11:30:19 -0600

    refactor of scripts package; introduction of scripts.export.all_dependencies

--- scripts/export.py
+++ scripts/export.py
@@ -1,35 +1,11 @@
-import subprocess
+from scripts import dependencies
+from scripts.enums import SubProjects
 
-from pathlib import Path
 
+def all_dependencies():
+    for subproject in SubProjects:
+        dependencies(subproject)
 
-PROJECTROOT = Path(__file__).parent.parent.absolute()
 
 def api_dependencies():
-    export_command = [
-        'python', '-m',
-        'poetry', 'export',
-        '--with=api',
-        '--without-hashes',
-        '--format=requirements.txt',
-    ]
-    depsfile = 'requirements.api.txt'
-    subprocess.run(
-        args=export_command,
-        stdout=Path(PROJECTROOT / depsfile).open(mode='w')
-    )
-
-
-def worker_dependencies():
-    export_command = [
-        'python', '-m',
-        'poetry', 'export',
-        '--with=worker',
-        '--without-hashes',
-        '--format=requirements.txt',
-    ]
-    depsfile = 'requirements.worker.txt'
-    subprocess.run(
-        args=export_command,
-        stdout=Path(PROJECTROOT / depsfile).open(mode='w')
-    )
+    dependencies(SubProjects.API)
